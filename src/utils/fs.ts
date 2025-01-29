1cb0b00e4a32c0e7a94b9caf673f0f8663e1bca0
kevinrodbe <kevinrodbe@users.noreply.github.com>
2025-01-29 01:44:52 -0500

    fix: validate exists path before read

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,8 +1,12 @@
-import { readFileSync } from 'node:fs';
+import { existsSync, readFileSync } from 'node:fs';
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
 export const readJsonFile = <Response>(filePath: string) => {
   try {
+    if (!existsSync(filePath)) {
+      return null;
+    }
+
     const file = readFileSync(filePath, 'utf8');
 
     return JSON.parse(file.toString()) as Response;
